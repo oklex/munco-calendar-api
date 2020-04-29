7e50e3c5ea7944e4d73fe0d8a9bbcc74199867f1
oklex <alexkim.347@gmail.com>
2020-04-28 18:30:02 -0700

    testing github actions

--- src/utils/GetData.ts
+++ src/utils/GetData.ts
@@ -3,9 +3,10 @@ import { MockCalendarData } from "../data/MockData";
 
 const getCalendarData = () => {
 	if (process.env.NODE_ENV == "development") {
-		// console.log("IN DEVELOPMENT: using mock data");
+		console.log("IN DEVELOPMENT: using mock data");
 		return MockCalendarData;
 	} else if (process.env.NODE_ENV == "production") {
+		console.log("IN PRODUCTION: using real data");
 		return AllCalendarData;
 	} else {
 		throw Error("NODE_ENV not specified");
