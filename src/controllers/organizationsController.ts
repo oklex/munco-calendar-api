15c07890b892f4369c90cf385921ee1f790d3e7b
oklex <alexkim.347@gmail.com>
2020-04-29 18:45:54 -0700

    prototype input check

--- src/controllers/organizationsController.ts
+++ src/controllers/organizationsController.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from "express";
+import { checkWebsite, checkName } from "../utils/CheckInput";
 
-export const post_new_organization = async function(req: Request, res: Response) {
-	res.send("PROTOTPYE ROUTE: /api/organizations/new")
-}
+export const post_new_organization = async function (
+	req: Request,
+	res: Response
+) {
+	try {
+		// check { req.body. {short_name, full_name, organization_type, website, running_since } }
+		// -> create a general purpose checking function that takes in "rules"
+		if (checkName(req.body.short_name) && checkName(req.body.full_name) && await checkWebsite(req.body.website)) {
+			console.log("all valid");
+			res.send("PROTOTPYE ROUTE: /api/organizations/new");
+		} else res.status(400).send("invalid input fields");
+		// 1 check DB for the uniqueness of candidate keys {website}
+		// 2 create default information? nope
+		// 3 fail if any data is missing
+		// 4 add to unique firebase route
+	} catch (err) {
+		// console.log(err)
+		res.status(500).send("internal error: " + err);
+	}
+};
---
+++ src/utils/CheckInput.ts
@@ -0,0 +1,28 @@
+//@ts-ignore
+import whois from "whois-promise";
+
+export let checkName = (name: string): boolean => {
+	if (name) {
+		let regex: RegExp = new RegExp(/^[a-zA-Z\s]*$/);
+		console.log("checkNames read string: ", name.match(regex).toString());
+		if (name == name.match(regex).toString()) return true;
+		else return false;
+	} else {
+		return false;
+	}
+};
+
+export let checkWebsite = async (website: string): Promise<boolean> => {
+	// check if it's unique (not on Firebase)
+	// check whois
+	if (website) {
+        return true
+		// let whoisResult: boolean = false;
+        // let result: any = await whois.json(website)
+		// if (result) whoisResult = true;
+		// else whoisResult = false;
+		// console.log("during promise: ", whoisResult);
+		// console.log("whois.lookup returns: ", website, result);
+		// return whoisResult;
+	} else return false;
+};
