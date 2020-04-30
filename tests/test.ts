a347601d9a26973c27c4ce43823f16ca47a018b0
oklex <alexkim.347@gmail.com>
2020-04-30 01:42:08 -0700

    updated test error condition

--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,7 @@ const dotenv = require("dotenv").config(); // required for process.env
 import "mocha";
 import { applicationsTest } from "./routeTests";
 
-if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV || process.env.GITHUB_ACTIONS) {
     applicationsTest();
 } else {
     throw Error("Error: NODE_ENV not found")
