9d0a95b668e1809c62e2dd45c52847a80d6f9f33
oklex <alexkim.347@gmail.com>
2020-05-02 18:54:59 -0700

    added firebase connection

--- src/controllers/organizationsController.ts
+++ src/controllers/organizationsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import firebase from "firebase";
 
 export const post_new_organization = async function (
 	req: Request,
@@ -7,6 +8,7 @@ export const post_new_organization = async function (
 	try {
 		// check { req.body. {short_name, full_name, organization_type, website, running_since } }
 		// -> create a general purpose checking function that takes in "rules"
+		firebase.database().ref('calendar/test' ).set(req.body)
 		res.send("PROTOTPYE ROUTE: /api/organizations/new");
 		// 1 check DB for the uniqueness of candidate keys {website}
 		// 2 create default information? nope
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 const dotenv = require("dotenv").config(); // required for process.env
 import getCalendarData from "./utils/GetData";
 import app from "./app";
+import FirebaseInitialize from "./database/Firebase";
 
 const port = process.env.PORT || 8081;
 
 try {
 	getCalendarData(); // ensure that NODE_ENV is valid
-	// FirebaseInitialize();
+	FirebaseInitialize();
 } catch (err) {
 	console.log(err);
 	throw err;
