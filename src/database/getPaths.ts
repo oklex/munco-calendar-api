b41642454a520ef94d1d98259c5101371ae79eb6
oklex <alexkim.347@gmail.com>
2020-05-06 22:55:50 -0700

    created a separate function for individual apps

--- src/database/getPaths.ts
+++ src/database/getPaths.ts
@@ -1,10 +1,15 @@
 import { getDomainKey } from "../utils/getDomain"
 import { calendarDataPath, orgApplicationsPath } from "./constants"
+import applicationRoute from "../routes/applications"
 
 export let getOrganizationPath = (website:string):string => {
     return calendarDataPath + '/' + getDomainKey(website)
 }
 
-export let getApplicationPath = (organizationSite: string):string => {
+export let getApplicationsPath = (organizationSite: string):string => {
     return calendarDataPath + '/' + getDomainKey(organizationSite) + orgApplicationsPath
+}
+
+export let getSingleApplicationPath = (organizationSite: string, applicationKey: string):string => {
+    return calendarDataPath + '/' + getDomainKey(organizationSite) + orgApplicationsPath + "/" + applicationKey
 }
