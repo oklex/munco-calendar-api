3968373831ae617b83e749b22c90cdc1c1f7c169
oklex <alexkim.347@gmail.com>
2020-05-07 16:07:44 -0700

    removed console logs

--- src/database/checkPaths.ts
+++ src/database/checkPaths.ts
@@ -4,13 +4,11 @@ export let checkPathNotNull = async (
 	path: string,
 	hasKey: string
 ): Promise<boolean> => {
-	console.log("check path: ", path);
 	let returnVal: boolean = await firebase
 		.database()
 		.ref(path)
 		.once("value")
 		.then(async (snapshot: any) => {
-			console.log("snapshot: ", snapshot);
 			return snapshot.hasChild(hasKey);
 		});
 	return returnVal;
