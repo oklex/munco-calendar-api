0ef2429a2013ef60f577ed312b0e02b296fafba6
oklex <alexkim.347@gmail.com>
2020-05-07 19:41:50 -0700

    added route prototypes for /events

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -1,8 +1,24 @@
 import { Router, Request, Response, NextFunction } from "express";
 const eventsRoute = Router();
 
+eventsRoute.post('/all', (req: Request, res: Response) => {
+	res.send("PROTOTPYE ROUTE: /api/events/all")
+})
+
+eventsRoute.post('/upcoming', (req: Request, res: Response) => {
+	res.send("PROTOTPYE ROUTE: /api/events/upcoming")
+})
+
 eventsRoute.post('/new', (req: Request, res: Response) => {
 	res.send("PROTOTPYE ROUTE: /api/events/new")
+})
+
+eventsRoute.patch('/:id', (req: Request, res: Response) => {
+	res.send("PROTOTPYE ROUTE: /api/events/:id")
+})
+
+eventsRoute.delete('/:id', (req: Request, res: Response) => {
+	res.send("PROTOTPYE ROUTE: /api/events/:id")
 })
 
 export default eventsRoute
