0cb05727e94f8c00fce3b2dc3d0b0a6b1f053d42
oklex <alexkim.347@gmail.com>
2020-05-08 19:40:18 -0700

    removed un-needed modules

--- src/controllers/organizations/orgGet.ts
+++ src/controllers/organizations/orgGet.ts
@@ -1,21 +1,12 @@
-import { Router, Request, Response, NextFunction } from "express";
-import findLargestAppEndDate from "../../utils/FindLargestAppEndDate";
-import {
-	ICalendarResponse,
-	IApplicationType,
-	IApplication,
-} from "../../models/CalendarResponse";
-import moment from "moment";
-import getCalendarData from "../../utils/GetData";
-import { dbPush } from "../../database/Firebase";
-import { getApplicationsPath } from "../../database/getPaths";
+import { Request, Response } from "express";
 
 export const organizations_get_all = async function (
 	req: Request,
 	res: Response
 ) {
 	try {
-        // map the results onto an array of Organization request
+		// map the results onto an array of Organization request
+		// get all the data from firebase; map for each object in the array -> onto response type
         res.send('prototype /organizations/ALL')
 	} catch (err) {
         res.status(500).send('GET failed')
@@ -29,7 +20,6 @@ export const organizations_get_byID = async function (
 	try {
         // map the results onto an obj of Organization request
         // if url params for include have "all", "applications", or "events" then include them
-        // 
 
         res.send('prototype /organizations/:id')
 	} catch (err) {
