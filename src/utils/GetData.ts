0c09f35e2e449039fe0c31a591b983a0bbdf68d0
oklex <alexkim.347@gmail.com>
2020-05-08 18:17:39 -0700

    added new check conditions for ENV

--- src/utils/GetData.ts
+++ src/utils/GetData.ts
@@ -2,17 +2,17 @@ import AllCalendarData from "../data/CalendarData";
 import { MockCalendarData } from "../data/MockData";
 
 const getCalendarData = () => {
-	if (process.env.NODE_ENV == "development") {
+	if (process.env.NODE_ENV == "development" && process.env.AUTH_TOKEN != null) {
 		// console.log("IN DEVELOPMENT: using mock data");
 		return MockCalendarData;
-	} else if (process.env.NODE_ENV == "production") {
+	} else if (process.env.NODE_ENV == "production"  && process.env.AUTH_TOKEN != null) {
 		// console.log("IN PRODUCTION: using real data");
 		return AllCalendarData;
-	} else if (process.env.GITHUB_ACTIONS == "true") { 
+	} else if (process.env.GITHUB_ACTIONS == "true") {
 		// console.log("IN TESTS: using real data");
 		return AllCalendarData;
-	}else {
-		throw Error("NODE_ENV not specified");
+	} else {
+		throw Error("ENV not specified; check NODE_ENV or AUTH_TOKEN");
 	}
 };
 
