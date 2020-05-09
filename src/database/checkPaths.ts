615910c40115fa4bb5f30372c72a1c9528530de0
oklex <alexkim.347@gmail.com>
2020-05-08 19:05:29 -0700

    always returns true when testing

--- src/database/checkPaths.ts
+++ src/database/checkPaths.ts
@@ -4,6 +4,7 @@ export let checkPathNotNull = async (
 	path: string,
 	hasKey: string
 ): Promise<boolean> => {
+	if (process.env.NODE_ENV !== "production") return true
 	let returnVal: boolean = await firebase
 		.database()
 		.ref(path)
