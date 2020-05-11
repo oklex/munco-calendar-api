12a8b4fe158dc1f773a95711d855eea088cd3149
oklex <alexkim.347@gmail.com>
2020-05-10 22:31:51 -0700

    renamed firebase ENV variables

--- src/database/Firebase.ts
+++ src/database/Firebase.ts
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 let FirebaseInitialize = () => {
 	let config: any = {
-		apiKey: process.env.FirebaseConfig_apiKey,
-		authDomain: process.env.FirebaseConfig_authDomain,
-		databaseURL: process.env.FirebaseConfig_databaseURL,
-		projectId: process.env.FirebaseConfig_projectId,
-		storageBucket: process.env.FirebaseConfig_storageBucket,
-		messagingSenderId: process.env.FirebaseConfig_messagingSenderId,
-		appId: process.env.FirebaseConfig_appId,
+		apiKey: process.env.FIREBASE_API_KEY,
+		authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+		databaseURL: process.env.FIREBASE_DATABASE_URL,
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.FIREBASE_SENDER_ID,
+		appId: process.env.FIREBASE_APP_ID,
 	};
 	try {
 		console.log("initializing FIrebase with: ", config);
