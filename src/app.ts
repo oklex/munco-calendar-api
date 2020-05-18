9d8211ae2f87f57ef4a81f0fea7e7db20b504d27
oklex <alexkim.347@gmail.com>
2020-05-17 18:29:26 -0700

    template for front-end display

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { Express, Request, Response, NextFunction } from "express";
 import express from "express";
 import bodyParser from "body-parser";
 import api from "./routes";
+import path from 'path'
 
 const app: Express = express();
 
@@ -16,5 +17,8 @@ app.use(
 );
 
 app.use("/api", api);
-
+// app.use("/", express.static(path.join(__dirname, '../web-build')))
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname, '../web-build', 'index.html'))
+// })
 export default app
