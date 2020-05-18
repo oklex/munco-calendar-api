d1cdc7ca03db70f2b69e68f7d505c5d0c70457d9
oklex <alexkim.347@gmail.com>
2020-05-17 19:02:22 -0700

    fixed https:// reading bug

--- src/utils/getDomain.ts
+++ src/utils/getDomain.ts
@@ -5,9 +5,16 @@ export let getDomain = (website: string) => {
 	return domain;
 };
 
-export let getDomainKey = (website: string) => {
-    let words: string[] = website.split(".");
-	if (words.length < 2) throw Error("domain is invalid");
-	let domain: string = words[words.length - 2] + "_" + words[words.length - 1];
-	return domain;
+export let getDomainKey = (link: string) => {
+	let split: string[] = link.split("//")
+	if (split.length > 2) {
+		throw Error("website is invalid")
+	} else {
+		let website: string = split[1]
+		website.replace("/", "")
+		let words: string[] = website.split(".");
+		if (words.length < 2) throw Error("domain is invalid: " + website + " - isn't - " + words[0]);
+		let domain: string = words[words.length - 2] + "_" + words[words.length - 1];
+		return domain;
+	}
 }
