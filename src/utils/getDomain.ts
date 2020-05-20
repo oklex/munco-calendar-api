29e9d1baa6e5edcc8b1d0e2dc1a938ba2b81addd
oklex <alexkim.347@gmail.com>
2020-05-20 16:53:35 -0700

    updated to work with or without "https://"

--- src/utils/getDomain.ts
+++ src/utils/getDomain.ts
@@ -9,12 +9,20 @@ export let getDomainKey = (link: string) => {
 	let split: string[] = link.split("//")
 	if (split.length > 2) {
 		throw Error("website is invalid")
-	} else {
+	} else if (split.length === 2){
 		let website: string = split[1]
 		website.replace("/", "")
 		let words: string[] = website.split(".");
 		if (words.length < 2) throw Error("domain is invalid: " + website + " - isn't - " + words[0]);
 		let domain: string = words[words.length - 2] + "_" + words[words.length - 1];
 		return domain;
+	} else if (split.length === 1) {
+		let website: string = split[0]
+		let words: string[] = website.split(".");
+		if (words.length < 2) throw Error("domain is invalid: " + website + " - isn't - " + words[0]);
+		let domain: string = words[words.length - 2] + "_" + words[words.length - 1];
+		return domain;
+	} else {
+		throw Error("website is invalid")
 	}
 }
