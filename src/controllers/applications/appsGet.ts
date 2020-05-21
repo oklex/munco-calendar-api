ee96581321514a12086748a785f9a5f66484ca58
oklex <alexkim.347@gmail.com>
2020-05-20 18:13:09 -0700

    updated /upcoming controller

--- src/controllers/applications/appsGet.ts
+++ src/controllers/applications/appsGet.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from "express";
-import findLargestAppEndDate from "../../utils/FindLargestAppEndDate";
+import {findLargestAppEndDate, findLargestAppStartDate } from "../../utils/FindLargestAppEndDate";
 import {
 	ICalendarResponse,
 	IApplicationType,
@@ -18,8 +18,8 @@ export const applications_get_all = async function (
 	res: Response
 ) {
 	try {
-		let data: any = await dbGetOnce(getOrganizationPath(""));
-		let responses: any = data.val()
+		let rawData: any = await dbGetOnce(getOrganizationPath(""));
+		let responses: any = rawData.val()
 		let keys: string[] = Object.keys(responses)
 		let returnResponses: ICalendarResponse[] = []
 		// console.log(keys, responses)
@@ -39,50 +39,17 @@ export const applications_get_all = async function (
 			return 0
 		})
 		)
-		console.log(returnResponses)
+		
+		returnResponses.sort((alpha, beta) => {
+			let alphaUpcomingDate: Date = findLargestAppStartDate(alpha);
+			let betaUpcomingDate: Date = findLargestAppStartDate(beta);
+			if (alphaUpcomingDate > betaUpcomingDate) return 1;
+			else if (alphaUpcomingDate < betaUpcomingDate) return -1;
+			else return 0;
+		});
+
+		console.log("returning: ", returnResponses)
 		res.send(returnResponses)
-		// let currentDate: Date = new Date();
-		// let data: ICalendarResponse[] = getCalendarData();
-		// let results: ICalendarResponse[] = [];
-		// await Promise.all(
-		// 	data.map(async (obj: ICalendarResponse) => {
-		// 		// delete all apps that have closed
-		// 		let tempApps: IApplication[] = [];
-		// 		await Promise.all(
-		// 			obj.applications.map((app) => {
-		// 				if (app.end_date > currentDate) {
-		// 					tempApps.push(app);
-		// 				}
-		// 			})
-		// 		).then(() => {
-		// 			results.push({
-		// 				organization: obj.organization,
-		// 				events: null,
-		// 				applications: tempApps,
-		// 			});
-		// 		});
-		// 		// obj.applications = tempApps;
-		// 	})
-		// )
-		// 	.then(async () => {
-		// 		// filter all objects with no apps
-		// 		let returnArray: ICalendarResponse[] = [];
-		// 		await Promise.all(
-		// 			results.map(async (obj: ICalendarResponse) => {
-		// 				if (obj.applications && obj.applications.length > 0)
-		// 					returnArray.push(obj);
-		// 			})
-		// 		).catch((err) => {
-		// 			res.status(500).send(err);
-		// 		});
-		// 		return returnArray;
-		// 	})
-		// 	.then((returnArray) => {
-		// 		res.send(returnArray);
-		// 	})
-		// 	.catch((err) => {
-		// 		res.status(500).send(err);
-		// 	});
 	} catch (err) {
 		res.status(500).send(err);
 	}
@@ -94,53 +61,34 @@ export const applications_get_upcoming = async function (
 ) {
 	console.log("api/applications/upcoming");
 	try {
-		let currentDate: Date = new Date();
-		let data: ICalendarResponse[] = getCalendarData();
-		let results: ICalendarResponse[] = [];
-		await Promise.all(
-			data.map(async (obj: ICalendarResponse) => {
-				// delete all apps that have closed
-				let tempApps: IApplication[] = [];
-				await Promise.all(
-					obj.applications.map((app) => {
-						if (app.end_date > currentDate) {
-							tempApps.push(app);
-						}
-					})
-				);
-				obj.applications = tempApps;
-				results.push(obj);
-			})
-		)
-			.then(async () => {
-				// filter all objects with no apps
-				let filteredByEmptyApps: ICalendarResponse[] = [];
-				await Promise.all(
-					results.map(async (obj: ICalendarResponse) => {
-						if (obj.applications && obj.applications.length > 0)
-							filteredByEmptyApps.push(obj);
-					})
-				).catch((err) => {
-					res.status(500).send(err);
-				});
-				return filteredByEmptyApps;
-			})
-			.then((returnArray) => {
-				returnArray.sort((alpha, beta) => {
-					let alphaUpcomingDate: Date = findLargestAppEndDate(alpha);
-					let betaUpcomingDate: Date = findLargestAppEndDate(beta);
-					if (alphaUpcomingDate > betaUpcomingDate) return 1;
-					else if (alphaUpcomingDate < betaUpcomingDate) return -1;
-					else return 0;
-				});
-				return returnArray;
-			})
-			.then((returnArray) => {
-				res.send(returnArray);
-			})
-			.catch((err) => {
-				res.status(500).send(err);
-			});
+		let rawData: any = await dbGetOnce(getOrganizationPath(""));
+		let responses: any = rawData.val()
+		let keys: string[] = Object.keys(responses)
+		let returnResponses: ICalendarResponse[] = []
+
+		await Promise.all(keys.map((key) => {
+			// console.log("accessing single obj: ", responses[key])
+			let response: ICalendarResponse = responses[key]
+			if (response.applications) {
+				returnResponses.push({
+					organization: MapOrgSnapshot(response, key),
+					events: [],
+					applications: MapAppArraySnapShot(response.applications)
+				})
+			}
+			return 0
+		}))
+
+		returnResponses.sort((alpha, beta) => {
+			let alphaUpcomingDate: Date = findLargestAppStartDate(alpha);
+			let betaUpcomingDate: Date = findLargestAppStartDate(beta);
+			if (alphaUpcomingDate > betaUpcomingDate) return 1;
+			else if (alphaUpcomingDate < betaUpcomingDate) return -1;
+			else return 0;
+		});
+
+		console.log("returning: ", returnResponses)
+		res.send(returnResponses.slice(0, 5))
 	} catch (err) {
 		res.status(500).send(err);
 	}
