3bed284794d436f2144343a3cba10dcef91ba444
oklex <alexkim.347@gmail.com>
2020-05-20 17:56:57 -0700

    added auth token middleware checks

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -1,23 +1,24 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { checkAuthToken } from "../middleware/checkAuthToken";
 const eventsRoute = Router();
 
-eventsRoute.post('/all', (req: Request, res: Response) => {
+eventsRoute.get('/all', (req: Request, res: Response) => {
 	res.send("PROTOTPYE ROUTE: /api/events/all")
 })
 
-eventsRoute.post('/upcoming', (req: Request, res: Response) => {
+eventsRoute.get('/upcoming', (req: Request, res: Response) => {
 	res.send("PROTOTPYE ROUTE: /api/events/upcoming")
 })
 
-eventsRoute.post('/new', (req: Request, res: Response) => {
+eventsRoute.post('/new', checkAuthToken, (req: Request, res: Response) => {
 	res.send("PROTOTPYE ROUTE: /api/events/new")
 })
 
-eventsRoute.patch('/:id', (req: Request, res: Response) => {
+eventsRoute.patch('/:id', checkAuthToken, (req: Request, res: Response) => {
 	res.send("PROTOTPYE ROUTE: /api/events/:id")
 })
 
-eventsRoute.delete('/:id', (req: Request, res: Response) => {
+eventsRoute.delete('/:id', checkAuthToken, (req: Request, res: Response) => {
 	res.send("PROTOTPYE ROUTE: /api/events/:id")
 })
 
