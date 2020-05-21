2e9f802398bc355b90240e72967126a9445c4928
oklex <alexkim.347@gmail.com>
2020-05-20 17:56:47 -0700

    updated /all route to use Firebase data

--- src/controllers/applications/appsGet.ts
+++ src/controllers/applications/appsGet.ts
@@ -4,11 +4,13 @@ import {
 	ICalendarResponse,
 	IApplicationType,
 	IApplication,
+	IOrganization,
 } from "../../models/CalendarResponse";
 import moment from "moment";
 import getCalendarData from "../../utils/GetData";
-import { dbPush } from "../../database/Firebase";
-import { getApplicationsPath } from "../../database/getPaths";
+import { dbPush, dbGetOnce } from "../../database/Firebase";
+import { getApplicationsPath, getOrganizationPath } from "../../database/getPaths";
+import { MapOrgSnapshot, MapAppArraySnapShot, MapAppArraySnapShotIfValid } from "../../utils/MapSnapShot";
 
 // get all
 export const applications_get_all = async function (
@@ -16,48 +18,71 @@ export const applications_get_all = async function (
 	res: Response
 ) {
 	try {
-		let currentDate: Date = new Date();
-		let data: ICalendarResponse[] = getCalendarData();
-		let results: ICalendarResponse[] = [];
-		await Promise.all(
-			data.map(async (obj: ICalendarResponse) => {
-				// delete all apps that have closed
-				let tempApps: IApplication[] = [];
-				await Promise.all(
-					obj.applications.map((app) => {
-						if (app.end_date > currentDate) {
-							tempApps.push(app);
-						}
-					})
-				).then(() => {
-					results.push({
-						organization: obj.organization,
-						events: null,
-						applications: tempApps,
-					});
-				});
-				// obj.applications = tempApps;
-			})
+		let data: any = await dbGetOnce(getOrganizationPath(""));
+		let responses: any = data.val()
+		let keys: string[] = Object.keys(responses)
+		let returnResponses: ICalendarResponse[] = []
+		// console.log(keys, responses)
+
+		await Promise.all(keys.map(async (key) => {
+			// console.log("accessing single obj: ", responses[key])
+			let response: ICalendarResponse = responses[key]
+			if (response.applications) {
+				let apps: IApplication[] = await MapAppArraySnapShotIfValid(response.applications)
+				console.log(apps)
+				returnResponses.push({
+					organization: MapOrgSnapshot(response, key),
+					events: [],
+					applications: apps
+				})
+			}
+			return 0
+		})
 		)
-			.then(async () => {
-				// filter all objects with no apps
-				let returnArray: ICalendarResponse[] = [];
-				await Promise.all(
-					results.map(async (obj: ICalendarResponse) => {
-						if (obj.applications && obj.applications.length > 0)
-							returnArray.push(obj);
-					})
-				).catch((err) => {
-					res.status(500).send(err);
-				});
-				return returnArray;
-			})
-			.then((returnArray) => {
-				res.send(returnArray);
-			})
-			.catch((err) => {
-				res.status(500).send(err);
-			});
+		console.log(returnResponses)
+		res.send(returnResponses)
+		// let currentDate: Date = new Date();
+		// let data: ICalendarResponse[] = getCalendarData();
+		// let results: ICalendarResponse[] = [];
+		// await Promise.all(
+		// 	data.map(async (obj: ICalendarResponse) => {
+		// 		// delete all apps that have closed
+		// 		let tempApps: IApplication[] = [];
+		// 		await Promise.all(
+		// 			obj.applications.map((app) => {
+		// 				if (app.end_date > currentDate) {
+		// 					tempApps.push(app);
+		// 				}
+		// 			})
+		// 		).then(() => {
+		// 			results.push({
+		// 				organization: obj.organization,
+		// 				events: null,
+		// 				applications: tempApps,
+		// 			});
+		// 		});
+		// 		// obj.applications = tempApps;
+		// 	})
+		// )
+		// 	.then(async () => {
+		// 		// filter all objects with no apps
+		// 		let returnArray: ICalendarResponse[] = [];
+		// 		await Promise.all(
+		// 			results.map(async (obj: ICalendarResponse) => {
+		// 				if (obj.applications && obj.applications.length > 0)
+		// 					returnArray.push(obj);
+		// 			})
+		// 		).catch((err) => {
+		// 			res.status(500).send(err);
+		// 		});
+		// 		return returnArray;
+		// 	})
+		// 	.then((returnArray) => {
+		// 		res.send(returnArray);
+		// 	})
+		// 	.catch((err) => {
+		// 		res.status(500).send(err);
+		// 	});
 	} catch (err) {
 		res.status(500).send(err);
 	}
