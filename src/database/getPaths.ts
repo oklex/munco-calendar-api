5262c67a5b5d5c6f3c5d0c8946d1fe0cfc298a8c
oklex <alexkim.347@gmail.com>
2020-05-20 18:29:53 -0700

    added paths for events

--- src/database/getPaths.ts
+++ src/database/getPaths.ts
@@ -1,27 +1,35 @@
 import { getDomainKey } from "../utils/getDomain"
-import { calendarDataPath, orgApplicationsPath } from "./constants"
+import { calendarDataPath, orgApplicationsPath, orgEventsPath } from "./constants"
 import applicationRoute from "../routes/applications"
 
-export let getOrganizationPathFromWebsite = (website:string):string => {
+export let getOrganizationPathFromWebsite = (website: string): string => {
     return calendarDataPath + '/' + getDomainKey(website)
 }
 
-export let getOrganizationPath = (website_key:string):string => {
+export let getOrganizationPath = (website_key: string): string => {
     return calendarDataPath + '/' + website_key
 }
 
-export let getApplicationsPath = (organizationSite: string):string => {
+export let getApplicationsPath = (organizationSite: string): string => {
     return calendarDataPath + '/' + getDomainKey(organizationSite) + orgApplicationsPath
 }
 
-export let getApplicationsPathWithKey = (website_key: string):string => {
+export let getApplicationsPathWithKey = (website_key: string): string => {
     return calendarDataPath + '/' + website_key + orgApplicationsPath
 }
 
-export let getSingleApplicationPath = (organizationSite: string, applicationKey: string):string => {
+export let getSingleApplicationPath = (organizationSite: string, applicationKey: string): string => {
     return calendarDataPath + '/' + getDomainKey(organizationSite) + orgApplicationsPath + "/" + applicationKey
 }
 
-export let getSingleApplicationPathWithKey = (website_key: string, applicationKey: string):string => {
+export let getSingleApplicationPathWithKey = (website_key: string, applicationKey: string): string => {
     return calendarDataPath + '/' + website_key + orgApplicationsPath + "/" + applicationKey
+}
+
+export let getEventsPathWithKey = (website_key: string) => {
+    return calendarDataPath + "/" + website_key + orgEventsPath
+}
+
+export let getSingleEventsPathWithKey = (website_key: string, eventKey: string) => {
+    return calendarDataPath + "/" + website_key + orgEventsPath + "/" + eventKey
 }
