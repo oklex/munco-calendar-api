10fd56a66c7aa7bf3119d21157123833be6744ec
oklex <alexkim.347@gmail.com>
2020-05-24 21:14:35 -0700

    fixed testing route name bug

--- tests/organizations/postTests.ts
+++ tests/organizations/postTests.ts
@@ -4,15 +4,16 @@ import request from "supertest";
 import app from "../../src/app";
 import { Request, Response, NextFunction } from "express";
 import { checkOrgValidInput } from "../../src/middleware/checkNewOrgInputs";
+import InitializeDatabase from "../../src/database/Firebase";
 
-export function organizationPostTests() {
+export async function organizationPostTests() {
 	return describe("Tests for POST and PATCH for organizations", () => {
 		context("input checker tests POST middleware: checkOrgValidInput", () => {
 			it("should not work if data is missing", (done) => {
 				app.post("/test/:website_key", checkOrgValidInput, (req: Request, res: Response) => {
 					res.status(200).send("success");
 				})
-				request(app).post("/test").expect(400, done);
+				request(app).post("/test/some_website").expect(400, done);
                 // expect(response.statusCode).tobe(500)
             });
 			it("should not work if data is missing", (done) => {
@@ -20,7 +21,7 @@ export function organizationPostTests() {
 					res.status(200).send("success");
 				})
 				request(app)
-					.post("/test")
+					.post("/test/some_website")
 					.send({
 						short_name: "nmun",
 						// full_name: "new model un",
