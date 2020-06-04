ac0a7c8beef92438559843272810874bfe4b40ac
oklex <alexkim.347@gmail.com>
2020-06-03 18:49:00 -0700

    serve static front-end

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ const dotenv = require("dotenv").config(); // required for process.env
 import { Request, Response } from "express";
 import app from "./app";
 import InitializeDatabase from "./database/Firebase";
+import path from 'path'
+import express from 'express'
 
 const port = process.env.PORT || 8081;
 
@@ -18,3 +20,10 @@ app.get("/admin", (req: Request, res: Response) => {
 app.listen(port, () => {
 	console.log("App is running at port " + port);
 });
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, '../web-build')))
+// Handle React routing, return all requests to React app
+app.get('*', function (req, res) {
+	res.sendFile(path.join(__dirname, '../web-build', 'index.html'))
+})
