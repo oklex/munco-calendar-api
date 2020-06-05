a4526a459f34f7881c6cf55df9c0e04f0af0646b
oklex <alexkim.347@gmail.com>
2020-06-04 20:46:38 -0700

    manual update to web-build

---
+++ web-build/precache-manifest.99e35e7f160c0b0bde8a76941eb31e53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228ee536afdf4ca2b40067d0fc9e33e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2b5cc614d97753e306b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f0ae9237259ae775ac4f",
+    "url": "/static/css/main.54a51da3.chunk.css"
+  },
+  {
+    "revision": "f2b5cc614d97753e306b",
+    "url": "/static/js/2.ef246927.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/static/js/2.ef246927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ae9237259ae775ac4f",
+    "url": "/static/js/main.3a1f2b32.chunk.js"
+  },
+  {
+    "revision": "723018217607c017709c",
+    "url": "/static/js/runtime-main.d86c73bb.js"
+  }
+]);
--- web-build/service-worker.js
+++ web-build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27156b3b9efd9a09b68b4c774d2efb78.js"
+  "/precache-manifest.99e35e7f160c0b0bde8a76941eb31e53.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
