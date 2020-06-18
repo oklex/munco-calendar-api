72aa5cfe57b0d10c4d99cdb5c1f022837eaa633c
oklex <alexkim.347@gmail.com>
2020-06-18 16:00:53 -0700

    changed to use env and not config json file

--- src/database/Firebase.ts
+++ src/database/Firebase.ts
@@ -28,7 +28,19 @@ let InitializeDatabase = () => {
 
 let initializeFirebaseAdmin = async () => {
 	try {
-		var serviceAccount = require("../../munco-calendar-firebase-adminsdk.json");
+		var serviceAccount: any = {
+			type: process.env.FIREBASE_ADMIN_TYPE,
+			project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+			private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+			private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
+			client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+			client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+			auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+			token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+			auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_AUTH_PROVIDER_URL,
+			client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_URL
+		};
+		console.log("serviceAccount: ", serviceAccount)
 		admin.initializeApp({
 			credential: admin.credential.cert(serviceAccount),
 			databaseURL: process.env.FIREBASE_DATABASE_URL
