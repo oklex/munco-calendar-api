ecf83e776168b649116eb66b6e0ebabb9eb584a9
oklex <alexkim.347@gmail.com>
2020-06-18 15:35:02 -0700

    created initial messaging function

---
+++ src/database/AdminMessaging.ts
@@ -0,0 +1,18 @@
+var admin = require("firebase-admin");
+import { IMPayload, IMOptions } from '../models/Messaging'
+
+export const FirebaseSendMessage = (tokens: string | string[], payload: IMPayload) => {
+    let options: IMOptions = {
+        ttl: 3600 
+    } // 1 hour by default
+    console.log("\n sending message(s)...", tokens, payload, '\n')
+    admin.messaging().sendToDevice(tokens, payload, options)
+        .then((response: any) => {
+            console.log("Successfully sent message:", response);
+        })
+        .catch((error: Error) => {
+            console.log("Error sending message:", error);
+        });
+}
+
+export default FirebaseSendMessage
