10eae415d8a4b51f543d1749b078379799d17ee7
oklex <alexkim.347@gmail.com>
2020-06-18 17:21:41 -0700

    manual update

---
+++ web-build/precache-manifest.06e09f2fe0975bc974217a91243a3968.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489dfcb62b5f7c32bc609b68c0863a56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6545d6cd600e78b6977b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "394704c98f8eb9029523",
+    "url": "/static/css/main.54a51da3.chunk.css"
+  },
+  {
+    "revision": "6545d6cd600e78b6977b",
+    "url": "/static/js/2.ad9b8cee.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/static/js/2.ad9b8cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394704c98f8eb9029523",
+    "url": "/static/js/main.7b1df8ee.chunk.js"
+  },
+  {
+    "revision": "723018217607c017709c",
+    "url": "/static/js/runtime-main.d86c73bb.js"
+  }
+]);
